eb6e2f73f43cca7d3ebe27293bf08958329b6800
fazeelabbasi <44741511+fazeelabbasi@users.noreply.github.com>
2019-04-01 13:54:46 -0400

    Code optimization + test file

--- Test/Test.ino
+++ Test/Test.ino
@@ -3,32 +3,36 @@
 
 //Robot pin declarations
 int rDir = 7;
-int lDir = 4;
 int rSpe = 6;
-int lSpe = 5;
-int leftWheelSpeed = 150;                             //set left wheel speed
-int rightWheelSpeed = 250;                            //set right wheel speed
-int IR = 2;                                          //used for acquiring initial position
-int bumper = 3;                                      //both bumpers are wired to the same circuit
+int lDir = 5;
+int lSpe = 4;
+
+
+
+//Robot wheel speeds
+int leftWheelSpeed = 175; //set left wheel speed
+int rightWheelSpeed = 175; //set right wheel speed
+
+int IR = 2; //used for acquiring initial position
+int bumper = 3; //both bumpers are wired to the same circuit
 int leftSensor = A3;
 int centerSensor = A4;
 int rightSensor = A5;
-int const THRESHOLD = 700;                           //threshold for light sensors
-int const turnDelay = 1000;                          //time for half turn
+int const THRESHOLD = 700; //threshold for light sensors
+int const turnDelay = 1000; //time for half turn
 int const speedChangeDelay = 200;
 
 Servo grip, tilt, pan;
 int panPin = 8;
 int gripPin = 10;
 int tiltPin = 9;
-int grabHeight = 90; //height for gripping
-int releaseHeight = 120; //height for releasing object
+int grabHeight = 70; //height for gripping
+int releaseHeight = 90; //height for releasing object
 int straightPan = 90; //angle of claw (facing forward)
 
 //open 0 - 180 close
 int holdDice = 120; //claw gripping state
 int releaseDice = 40; //claw open state
-
 
 //Configuring robot turning conditions
 int P1[] = {6, 9, 4, 9, 6, 3, 9, 1, 3, 9, 6, 9, 2, 9, 5, 9, 2, 9, 2, 9, 2, 9}; //Position 1 [22]
@@ -41,7 +45,7 @@ int intersections[25] = {};
 int turns[25] = {}; 
 
 //Other variables
-int count = 0; //used to count intersections traversed
+int count = 1; //used to count intersections traversed
 int action = 0; //used to traverse above arrays
 boolean clawState = false; //used to determine if claw should be closed or open
 int ball = 0; //denotes turn scenarios i.e. ball = 0 means going for first object, ball = 1 means going for second object, etc.
@@ -56,7 +60,7 @@ void setup() {
   }
 
 //  while(!getPosition){ //robot will not run loop code until initial position has been acquired
-    int signal = 48; //temporaily assign IR variable [ONLY FOR TESTING PURPOSES]
+    int signal = 48;
 //    int signal = digitalRead(IR);
 //    if (signal == LOW){
       checkPosition(signal);
@@ -76,23 +80,18 @@ void setup() {
   pinMode(lDir, OUTPUT); 
   pinMode(rSpe, OUTPUT); 
   pinMode(lSpe, OUTPUT);
+  
 //
 //  //set up sensor
 //  pinMode(leftSensor, INPUT);
 //  pinMode(centerSensor, INPUT);
 //  pinMode(rightSensor, INPUT);
   
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  driveForward();                  //drive forwards
-  checkBumper();
-  checkClaw();
-  checkIntersection();
-  checkEnd();  
+  setDRIVE(100, 50);
 }                    
 
 //LOGIC FUNCTIONS TO EXECUTE DIRECTIONS
@@ -106,7 +105,7 @@ void checkClaw(){                               //checking claw state ([0] open 
 }
 
 boolean checkSensor(int sensor){
-  if (analogRead(sensor) > THRESHOLD){
+  if (sensor > THRESHOLD){
     return true;
   }
 }
@@ -119,7 +118,7 @@ void checkIntersection(){                       //check if robot is passing inte
     Serial.println(analogRead(rightSensor));
      Serial.println("--------------------SENSOR END LCR---------------------");
 //  if ((checkSensor(analogRead(leftSensor)) == true )&&( checkSensor(analogRead(rightSensor)) == true)) {    //when robot passes intersection
-   if ((analogRead(leftSensor) > THRESHOLD) && (analogRead(rightSensor) > THRESHOLD)) {
+   if ((analogRead(leftSensor) > THRESHOLD) && (analogRead(rightSensor > THRESHOLD))) {
     count++;
     delay(1000);
   }
@@ -131,26 +130,26 @@ void checkTurn(){                               //check if robot needs to turn
   if (intersections[action] == count){         
     halfTurn(turns[action]);
     action++;
-    count = 0;
+    count = 1;
   }
 }
 
 void checkBumper(){                            //check if robot impacts wall
   if (bumper == 1) {                         
-     if (clawState == false){
+     action++;
+     if (clawState = false){
         backUp();
         grab(); //close claw function
         clawState = !clawState;
      }    
-     else if (clawState == true){
+     else if (clawState = true){
+        backUp();
         openGripper();           //open claw function
         clawState = !clawState;
         ball++;
-        backUp();
      }              
-     count = 0;  
+     count = 1;  
      turn180degrees();               //reverse and turn function
-     action++;
   }  
 }     
 
@@ -183,72 +182,43 @@ void checkPosition(int val){           //When IR signal is received, checks for 
 }
 
 //DRIVE FUNCTIONS FOR ROBOT
-void setSpeed(int leftSPEED, int rightSPEED) {
-  if (leftSPEED < 0) {
-    digitalWrite(lDir, LOW);
-    leftSPEED = -leftSPEED;
-  }
-  else {
-    digitalWrite(lDir, HIGH);
-  }
-  if (rightSPEED == 0){
-    digitalWrite(rDir, LOW);
-  }
-  else {
-    digitalWrite(rDir, HIGH);
-  }
+void setDRIVE(int leftPOW, int rightPOW) {
+  if (leftPOW == 0)digitalWrite(lDir, LOW);
+  else digitalWrite(lDir, HIGH);
+  if (rightPOW == 0)digitalWrite(rDir, LOW);
+  else digitalWrite(rDir, HIGH);
 
-//  leftSPEED /= 2;
-//  leftSPEED += 128;
+  leftPOW/=2;
+  leftPOW+=128;
 
-  rightSPEED /= 2;
+  rightPOW/=2;
+  rightPOW+=128;
   
-  if (rightSPEED > 0){
-    rightSPEED += 128;
-  }
-  
-  analogWrite(rSpe, rightSPEED);
-  analogWrite(lSpe, leftSPEED);
-  Serial.println("Left Speed:");
-  Serial.println(leftSPEED);
-  Serial.println("Right Speed:");
-  Serial.println(rightSPEED);
-}
-
-void driveForward() {
-  setSpeed(leftWheelSpeed, rightWheelSpeed);
-//  if ((analogRead(rightSensor) > THRESHOLD) && (analogRead(centerSensor) < THRESHOLD) && (analogRead(leftSensor) < THRESHOLD))
-//  {
-//     Serial.println("Corecting LEFT BIAS");
-//    analogWrite(rSpe, rightWheelSpeed - 15);
-//    analogWrite(lSpe, leftWheelSpeed + 15);
-//    delay(speedChangeDelay);
-//  }
-//  else if ((analogRead(leftSensor) > THRESHOLD) && (analogRead(centerSensor) < THRESHOLD) && (analogRead(rightSensor) < THRESHOLD))
-//  {
-//    Serial.println("Corecting RIGHT BIAS");
-//    analogWrite(rSpe, rightWheelSpeed + 15);
-//    analogWrite(lSpe, leftWheelSpeed - 15);
-//    delay(speedChangeDelay);
-//  }
+  analogWrite(rSpe, rightPOW);
+  analogWrite(lSpe, leftPOW);
 }
 
 void stopDriving(){
-  setSpeed(0, 0);
+  digitalWrite(rDir, HIGH); 
+  digitalWrite(lDir, HIGH); 
+  analogWrite(rSpe, 0);
+  analogWrite(lSpe, 0);
 }
 
 void halfTurn(int dir){
-  if(dir == 0){               //left turn
-//     while(!checkSensor(leftSensor)){
-      setSpeed(-leftWheelSpeed, rightWheelSpeed);
-      delay(turnDelay);
-//     }
+  if(dir == 0){ 
+    digitalWrite(rDir, HIGH); 
+    digitalWrite(lDir, HIGH); 
+    analogWrite(rSpe, rightWheelSpeed);
+    analogWrite(lSpe, -leftWheelSpeed);
+    delay(turnDelay);
   }
-  else if (dir == 1){         //right turn
-//     while(!checkSensor(rightSensor)){
-      setSpeed(leftWheelSpeed, -rightWheelSpeed);
-      delay(turnDelay);
-//     }
+  else if (dir == 1){
+    digitalWrite(rDir, HIGH); 
+    digitalWrite(lDir, HIGH); 
+    analogWrite(rSpe, -rightWheelSpeed);
+    analogWrite(lSpe, leftWheelSpeed);
+    delay(turnDelay);
   } 
 }
 
@@ -256,15 +226,20 @@ void backUp(){
   int counter = 0;
   counter++;
   while(counter < 500){
-  setSpeed(-leftWheelSpeed, -rightWheelSpeed);
+  digitalWrite(rDir, LOW); 
+  digitalWrite(lDir, LOW); 
+  analogWrite(rSpe, -rightWheelSpeed);
+  analogWrite(lSpe, -leftWheelSpeed);
   }
 }
 
 void turn180degrees(){
-//   while(!checkSensor(centerSensor)){
-    setSpeed(-leftWheelSpeed, rightWheelSpeed);
-    delay(2*turnDelay);
-//   } 
+  while(!checkSensor(centerSensor)){
+    digitalWrite(rDir, HIGH); 
+    digitalWrite(lDir, LOW); 
+    analogWrite(rSpe, rightWheelSpeed);
+    analogWrite(lSpe, -leftWheelSpeed);
+  } 
 }
 
 //GRIP FUNCTIONS FOR ROBOT
